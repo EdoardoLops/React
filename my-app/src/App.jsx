69cc1264a266a3bdd3acf20b81a6ec5cd1c3c110
edoardo <edolops.el@gmail.com>
2024-12-05 22:59:45 +0100

    Props

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,43 +1,14 @@
-import { useState } from 'react'
 import './App.css'
-import { FilmButton } from './FilmButton';
-import { Film } from './film';
+import { Age, Welcome } from './welcome'
 
 function App() {
-
-  const filmArray = ["Matrix", "Inception", "Titanic", "Lord of The Rings", "Star Wars"];
-  
-  const [preferiti, setPreferiti] = useState([])
-
-  function AggiungiPreferiti(array) {
-    if (!preferiti.includes(array)) {
-      setPreferiti([...preferiti, array])
-    }
-  }
-
-  function RimuoviPreferiti(array) {
-    if (preferiti.includes(array)) {
-      setPreferiti([...preferiti])
-    }
-  }
-  
-  return (
+  return ( 
     <>
-      <div>
-        <h1>Film</h1>
-        <ul>
-          {filmArray.map((x, index) => (
-            <Film key={index} titolo={x} stato={preferiti.includes(x)} add={AggiungiPreferiti} remove={RimuoviPreferiti} />
-          ))}
-        </ul>
-        <h2>Preferiti</h2>
-        <div>
-          {preferiti.length < 0? (<ul>{preferiti.map((x, index)=> (<li key={index}>{x}</li>))}</ul>) : (<p>Aggiungi un film tra i preferiti</p>)} 
-        </div>
-      </div>
+      <Welcome name="Marco" />
+      <Age age="21" />
     </>
+    
   )
-
 }
 
 export default App
---
+++ my-app/src/welcome.jsx
@@ -0,0 +1,7 @@
+export function Welcome(props) {
+  return <p>Welcome, <strong>{props.name}</strong>!</p>
+}
+
+export function Age(props) {
+    return <p>Your age is {props.age}</p>
+}
