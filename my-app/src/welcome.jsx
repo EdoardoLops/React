a41c2a4048512d8ebf6289a7fb160f195521e5fc
edoardo <edolops.el@gmail.com>
2024-12-06 14:41:36 +0100

    finire compito

--- my-app/src/welcome.jsx
+++ my-app/src/welcome.jsx
@@ -1,7 +1,3 @@
-export function Welcome(props) {
-  return <p>Welcome, <strong>{props.name}</strong>!</p>
-}
-
-export function Age(props) {
-    return <p>Your age is {props.age}</p>
+export function Welcome(prop) {
+    
 }
